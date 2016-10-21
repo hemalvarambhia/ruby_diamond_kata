bf57f1b1c4a306045d4b3444edd23fe40f21a6fe
Hemal <h.n.varambhia@gmail.com>
2016-10-21 12:55:47 +0100

    Test list - for the letter 'A', it correctly prints the full diamond.

--- spec/diamond_spec.rb
+++ spec/diamond_spec.rb
@@ -2,4 +2,8 @@ describe 'Generating a diamond' do
   context 'when no letter is given' do
     it 'prints no diamond'
   end
+
+  context "given the letter 'A'" do
+    it 'prints the diamond correctly'
+  end
 end
