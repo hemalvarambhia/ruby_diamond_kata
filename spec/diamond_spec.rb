aaaf1922ca7d404aa92a87357c4f170229a73f18
Hemal <h.n.varambhia@gmail.com>
2016-10-21 13:39:05 +0100

    Test list - we now focus on the just printing the left-hand corner of any diamond, so removed the examples for the other corners & added an example of the corner of diamond for the letter D.

--- spec/diamond_spec.rb
+++ spec/diamond_spec.rb
@@ -21,15 +21,13 @@ describe 'Generating a diamond' do
 
   context "given the letter 'B'" do
     it 'correctly prints the top left-hand corner'
-
-    it 'correctly prints the top right-hand corner'
-
-    it 'correctly prints the bottom left-hand corner'
-
-    it 'correctly prints the bottom right-hand corner'
   end
 
   context "given the letter 'C'" do
+    it 'correctly prints the top left-hand corner'
+  end
+
+  context "given the letter 'D'" do
     it 'correctly prints the top left-hand corner'
   end
 end
