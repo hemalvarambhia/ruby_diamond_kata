3401d4e93913ec8abfc25f3f4d0fa6f0286e9b8b
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-10-29 17:05:04 +0100

    Changed the interface - now each row is a string.

--- spec/diamond_spec.rb
+++ spec/diamond_spec.rb
@@ -25,7 +25,7 @@ describe 'Generating a diamond' do
       
       range = 'A'..letter
       left_hand_corner =
-        Array.new(range.count) {  [' '] * range.count }
+        Array.new(range.count) {  ' ' * range.count }
 
       range.each_with_index do |letter, index|
         left_hand_corner[index][(range.count - 1) - index] = letter
@@ -44,7 +44,7 @@ describe 'Generating a diamond' do
       it 'is correctly printed' do
         left_hand_corner = left_hand_corner 'A'
 
-        expect(left_hand_corner[0]).to eq ['A']
+        expect(left_hand_corner[0]).to eq 'A'
       end
     end
 
@@ -52,8 +52,8 @@ describe 'Generating a diamond' do
       it 'is printed correctly' do
         left_hand_corner = left_hand_corner 'B'
 
-        expect(left_hand_corner[0]).to eq [' ', 'A']
-        expect(left_hand_corner[1]).to eq ['B', ' ']
+        expect(left_hand_corner[0]).to eq ' A'
+        expect(left_hand_corner[1]).to eq 'B '
       end
     end
 
@@ -61,9 +61,9 @@ describe 'Generating a diamond' do
       it 'is printed correctly' do
         left_hand_corner = left_hand_corner 'C'
 
-        expect(left_hand_corner[0]).to eq [' ', ' ', 'A']
-        expect(left_hand_corner[1]).to eq [' ', 'B', ' ']
-        expect(left_hand_corner[2]).to eq ['C', ' ', ' ']
+        expect(left_hand_corner[0]).to eq '  A'
+        expect(left_hand_corner[1]).to eq ' B '
+        expect(left_hand_corner[2]).to eq 'C  '
       end
     end
 
@@ -71,10 +71,10 @@ describe 'Generating a diamond' do
       it 'is printed correctly' do
         left_hand_corner = left_hand_corner 'D'
 
-        expect(left_hand_corner[0]).to eq [' ', ' ', ' ', 'A']
-        expect(left_hand_corner[1]).to eq [' ', ' ', 'B', ' ']
-        expect(left_hand_corner[2]).to eq [' ', 'C', ' ', ' ']
-        expect(left_hand_corner[3]).to eq ['D', ' ', ' ', ' ']
+        expect(left_hand_corner[0]).to eq '   A'
+        expect(left_hand_corner[1]).to eq '  B '
+        expect(left_hand_corner[2]).to eq ' C  '
+        expect(left_hand_corner[3]).to eq 'D   '
       end
     end
   end
