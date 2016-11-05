bd30eb60677da9b86fb8303a0630d419171709c0
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-11-05 22:05:10 +0000

    Moved the methods to a more sensible place in the codebase.

--- spec/diamond_spec.rb
+++ spec/diamond_spec.rb
@@ -1,43 +1,5 @@
+require 'diamond'
 describe 'Generating a diamond' do
-  def top_left_hand_corner letter
-    return [] if letter.empty?
-     
-    range = 'A'..letter
-    top_left_hand_corner =
-      Array.new(range.count) {  ' ' * range.count }
-
-    range.each_with_index do |letter, index|
-      top_left_hand_corner[index][(range.count - 1) - index] = letter
-    end
-
-    top_left_hand_corner
-  end
-
-  def top_right_hand_corner letter
-    top_left_hand_corner(letter).map { |row| row.reverse }
-  end
-
-  def upper_half letter
-    return [] if letter == ''
-    finish = ('A'..'Z').to_a.index letter
-
-    (0.upto finish).map do |index|
-       top_left_hand_corner(letter)[index] +
-         top_right_hand_corner(letter)[index][1..-1]
-     end
-  end
-
-  def lower_half letter
-    upper_half(letter).reverse
-  end
-
-  def diamond letter
-    return '' if letter == ''
-      
-    upper_half(letter).join("\n") + "\n" +
-      lower_half(letter)[1..-1].join("\n")
-  end
-  
   describe 'the left-hand corner' do
     context 'given no letter' do
       it 'prints no left-hand corner' do
